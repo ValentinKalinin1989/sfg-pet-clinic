a738150658ed6ca9200ca0e1792079567f6f1015
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-06-09 20:01:22 +0300

    Added Specialities, added to Vet. Closes #27.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specilty> specilties;
+
+    public Set<Specilty> getSpecilties() {
+        return specilties;
+    }
+
+    public void setSpecilties(Set<Specilty> specilties) {
+        this.specilties = specilties;
+    }
 }
