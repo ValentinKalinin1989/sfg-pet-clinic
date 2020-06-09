7518e797ff1fd8764098341d758d4dd247263563
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-06-10 00:01:57 +0300

    Added Pets and Contract info to owners on startup. Closes #33.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Specilty> specilties;
+    private Set<Specilty> specilties = new HashSet<>();
 
     public Set<Specilty> getSpecilties() {
         return specilties;
